52e8ac5dfea8f462b52cb79b9822649c386b1899
iamseki <chrisyuri_19@hotmail.com>
2020-10-17 23:49:21 -0300

    Add mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
+import { CollegeModule } from './college/college.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/grades';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(MONGO_URI), CollegeModule],
 })
-export class AppModule {}
+export class AppModule { }
