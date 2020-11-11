ef073c7bb858e86d97e72869e5ce4bf14f626020
iamseki <chrisyuri_19@hotmail.com>
2020-11-10 22:59:30 -0300

    Add docker-compose and Scripts Dockerfile

--- scripts/main.go
+++ scripts/main.go
@@ -5,18 +5,22 @@ import (
 	"flag"
 	"log"
 	"sync"
+	"time"
 )
 
 func main() {
 	var boolSeed bool
 	var table string
+	var delay int
 	var wg sync.WaitGroup
 
 	flag.BoolVar(&boolSeed, "seed", false, "Execute seed script if flag is passed e.g(-seed)")
 	flag.StringVar(&table, "table", "none", "Table passed to execute query")
+	flag.IntVar(&delay, "delay", 1, "Delay to wait to start the go program")
 
 	flag.Parse()
 
+	time.Sleep(time.Duration(delay) * time.Second)
 	// execute seed script if running with -seed -table=name
 	// go run main.go -seed will populate all .json availables
 	if boolSeed {
