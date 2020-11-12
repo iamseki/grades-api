8d7c7aefcf4694d6a4842648475c1349a95e096b
iamseki <chrisyuri_19@hotmail.com>
2020-11-12 00:26:13 -0300

    Update api-scripts bin

--- scripts/database/migration.go
+++ scripts/database/migration.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"bytes"
+	"log"
 	"os"
 	"os/exec"
 )
@@ -10,13 +11,16 @@ type migration struct {
 }
 
 func (m *migration) Execute(table string) {
-
+	if table == "none" {
+		log.Println("Running migrations..")
+		runMigrationCMD()
+		log.Println("Finished migrations...")
+	}
 }
 
 func runMigrationCMD() error {
 	cmd := exec.Command("cd", os.Getenv("APP_DIR"), "&&",
-		"yarn", "add", "ts-node", "tsconfig-paths", "&&",
-		"yarn", "typeorm", "migration:run")
+		"yarn", "typeorm:script-migration")
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err := cmd.Run()
