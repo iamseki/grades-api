70a919177f7a7b46bfffb3ec0c97641a0279d0ed
iamseki <chrisyuri_19@hotmail.com>
2020-11-12 22:45:20 -0300

    Add JWT resources

---
+++ src/app.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from './modules/auth/local-auth.guard';
+import { AuthService } from './modules/auth/auth.service';
+
+@Controller()
+export class AppController {
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    const { username, password } = req.user; 
+    return this.authService.login({ username, password });
+  }
+
+  @Get('hc')
+  healthCheck() {
+    return { health: "ok"}
+  }
+}
