aa7f006338080a5fa951d199976af0b318a0094d
iamseki <chrisyuri_19@hotmail.com>
2020-12-01 23:36:00 -0300

    Add Database Production config

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -3,36 +3,58 @@ import * as path from 'path';
 
 const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env;
 
-const config: ConnectionOptions = {
-  type: 'postgres',
-  host: DB_HOST,
-  port: Number(DB_PORT),
-  username: DB_USERNAME,
-  password: DB_PASSWORD,
-  database: DB_DATABASE || 'grades-api',
-  entities: ['dist/modules/**/entities/*.entity{.ts,.js}'],
+let config: ConnectionOptions
 
-  // We are using migrations, synchronize should be set to false.
-  synchronize: false,
+if (DB_HOST) {
+  config = {
+    type: 'postgres',
+    host: DB_HOST,
+    port: Number(DB_PORT),
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE || 'grades-api',
+    entities: ['dist/modules/**/entities/*.entity{.ts,.js}'],
 
-  // Do not run migrations automatically,
-  // you can disable this if you prefer running migration manually.
-  migrationsRun: false,
+    // We are using migrations, synchronize should be set to false.
+    synchronize: false,
 
-  // A file named ormlogs.log will be generated in the root folder.
-  logging: true,
-  logger: 'file',
+    // Do not run migrations automatically,
+    // you can disable this if you prefer running migration manually.
+    migrationsRun: false,
 
-  // Allow both start:prod and start:dev to use migrations
-  // __dirname is either dist or src folder, meaning either
-  // the compiled js in prod or the ts in dev.
-  migrations: [path.join(__dirname, '..', 'database/migrations/**/*{.ts,.js}')],
-  cli: {
-    // Location of migration should be inside src folder
-    // to be compiled into dist/ folder.
-    migrationsDir: 'src/database/migrations',
-  },
-};
+    // A file named ormlogs.log will be generated in the root folder.
+    logging: true,
+    logger: 'file',
+
+    // Allow both start:prod and start:dev to use migrations
+    // __dirname is either dist or src folder, meaning either
+    // the compiled js in prod or the ts in dev.
+    migrations: [path.join(__dirname, '..', 'database/migrations/**/*{.ts,.js}')],
+    cli: {
+      // Location of migration should be inside src folder
+      // to be compiled into dist/ folder.
+      migrationsDir: 'src/database/migrations',
+    },
+  };
+} else {
+  const { DATABASE_URL } = process.env;
+  config = {
+    type: 'postgres',
+    url: DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+    entities: ['dist/modules/**/entities/*.entity{.ts,.js}'],
+    connectTimeoutMS: 2000,
+    synchronize: false,
+    migrationsRun: false,
+    logging: true,
+    logger: 'file',
+
+    migrations: [path.join(__dirname, '..', 'database/migrations/**/*{.ts,.js}')],
+    cli: {
+      migrationsDir: 'src/database/migrations',
+    },
+  };
+}
 
 // If you need to debug typeorm cli you can do it here
 //console.log(config.migrations)
